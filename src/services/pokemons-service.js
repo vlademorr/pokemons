931f1638590243999ebec40f077898d0cd6abdd0
vlademorr <47037960+vlademorr@users.noreply.github.com>
2020-03-29 16:50:29 +0200

    Update pokemons-service.js

--- src/services/pokemons-service.js
+++ src/services/pokemons-service.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export default class PokemonsService {
     getPokemons(loadNewItems) {
         if (loadNewItems) {
-            return axios.get(`http://pokeapi.co/api/v2/pokemon/?limit=12&offset=${loadNewItems}`)
+            return axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=12&offset=${loadNewItems}`)
         }
-        return axios.get('http://pokeapi.co/api/v2/pokemon/?limit=12')
+        return axios.get('https://pokeapi.co/api/v2/pokemon/?limit=12')
     }
 }
